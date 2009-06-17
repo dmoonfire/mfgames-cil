ea6780ba476020ea2edeafe67617c5b56f32404c
dmoonfire <dmoonfire@84af6989-d30b-0410-891c-c1f6a83ca0db>
2009-06-17 01:53:23 +0000

    * Created the System.Object extensions with use with the ExtendedConvert. Closes #0000020
    git-svn-id: http://svn.mfgames.com/MfGames/trunk@1045 84af6989-d30b-0410-891c-c1f6a83ca0db

---
+++ MfGames/Extensions/SystemObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MfGames.Utility;
+
+namespace MfGames.Extensions
+{
+	/// <summary>
+	/// Extension methods for System.Object.
+	/// </summary>
+	public static class SystemObjectExtensions
+	{
+		/// <summary>
+		/// Changes the type of the given object to another type, using the ExtendedConvert.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="value">The value.</param>
+		/// <returns></returns>
+		public static T ChangeType<T>(this object value)
+		{
+			return (T) ExtendedConvert.ChangeType(value, typeof(T));
+		}
+	}
+}
