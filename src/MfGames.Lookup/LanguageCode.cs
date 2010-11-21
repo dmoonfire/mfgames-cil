7c95a1e45bb73853eccfc5b2aefd7ee369c0c678
D. Moonfire <d.moonfire@mfgames.com>
2010-11-20 19:35:56 -0600

    Added a stub for language code processing. And a new lookup assembly for the static lookup things.

---
+++ src/MfGames.Lookup/LanguageCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MfGames.Lookup
+{
+	/// <summary>
+	/// Represents a language code in normalized format. A language consists of
+	/// a ISO 639-3 language code along with an optional script and region codes
+	/// as described in RFC 5646 (http://tools.ietf.org/html/rfc5646).
+	/// </summary>
+	public class LanguageCode
+	{
+		#region Codes
+
+		public string Language { get; set; }
+
+		#endregion
+	}
+}
