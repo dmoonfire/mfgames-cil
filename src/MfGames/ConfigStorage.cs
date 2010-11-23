aede6c97971baa641d720150b85c24a1080f4ae8
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2010-11-22 21:24:48 -0600

    Updating the ConfigStorage class.

--- src/MfGames/ConfigStorage.cs
+++ src/MfGames/ConfigStorage.cs
@@ -35,188 +35,84 @@ using MfGames.Logging;
 namespace MfGames
 {
 	/// <summary>
-	/// This implements a static, singleton-style class that managed a
-	/// configuration or customization area. It allows for
-	/// $HOME/.MfGames/ on Unix/Linux while creating a folder called
-	/// MfGames in the "Application Data" folder on Windows.
+	/// Contains a series of static methods used to finding and managing directories
+	/// and files inside the configuration area. On a Unix platform, this will be
+	/// located in $HOME/.config and in Application Data on the Windows platform.
 	/// </summary>
-	public class ConfigStorage
+	public static class ConfigStorage
 	{
-		#region Constructor
-
 		/// <summary>
-		/// Creates a new storage constructor with the given key.
+		/// Contains the configuration directory for the current user.
 		/// </summary>
-		public ConfigStorage(string storageKey)
-		{
-			// Save the field, this also creates the storage.
-			StorageName = storageKey;
-		}
-
-		#endregion
-
-		#region Singleton
-
-		/// <summary>
-		/// Gets or sets the application directory which can be stored by the application.
-		/// </summary>
-		/// <value>The application directory.</value>
-		public static DirectoryInfo ApplicationDirectory { get; set; }
-
-		/// <summary>
-		/// Contains the singleton storage as defined by the
-		/// application. This is null unless defined by the application.
-		/// </summary>
-		public static ConfigStorage Instance { get; set; }
-
-		#endregion
-
-		#region Storage Key
-
-		// Contains the regex used to verify the storage key
-		private static readonly Regex ValidateStorageKey = new Regex("^[\\w\\d ]+$");
-
-		// Contains the key which defines where/what type of config
-		// storage to create. This is used as the directory name of the
-		// top level and many of the functions will throw an exception if
-		// this is not defined.
-		private string storageName;
-
-		/// <summary>
-		/// Getter and setter for StorageName. A storage key can consist of
-		/// spaces, letters, and numbers, but no other special
-		/// characters. It is case-sensitive.
-		/// </summary>
-		public string StorageName
+		public static string ConfigurationDirectory
 		{
 			get
 			{
-				// Return the key
-				return storageName;
+				return Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 			}
-			set
+		}
+		
+		/// <summary>
+		/// Contains the configuration directory for the current user.
+		/// </summary>
+		public static DirectoryInfo ConfigurationDirectoryInfo
+		{
+			get { return new DirectoryInfo(ConfigurationDirectory); }
+		}
+
+		/// <summary>
+		/// Retrieves, creating if needed, a directory inside the user's configuration area.
+		/// </summary>
+		public static DirectoryInfo GetDirectoryInfo(
+			params string[] relativeDirectories)
+		{
+			return GetDirectoryInfo(true, relativeDirectories);
+		}
+		
+		/// <summary>
+		/// Retrieves, creating if needed, a directory inside the user's configuration area.
+		/// </summary>
+		public static string GetDirectory(
+			params string[] relativeDirectories)
+		{
+			return GetDirectoryInfo(true, relativeDirectories).FullName;
+		}
+		
+		/// <summary>
+		/// Retrieves, creating if requested, a directory inside the user's configuration area.
+		/// </summary>
+		public static string GetDirectory(
+		    bool create,
+			params string[] relativeDirectories)
+		{
+			return GetDirectoryInfo(create, relativeDirectories).FullName;
+		}
+		
+		/// <summary>
+		/// Retrieves, creating if requested, a directory inside the user's configuration area.
+		/// </summary>
+		public static DirectoryInfo GetDirectoryInfo(
+		    bool create,
+			params string[] relativeDirectories)
+		{
+			// Start at the top-level configuration directory and create all the relative
+			// directories before returning the inner-mode directory.
+			DirectoryInfo directory = ConfigurationDirectoryInfo;
+			
+			foreach (string relativeDirectory in relativeDirectories)
 			{
-				// If we are null or blank, then just clear it.
-				if (string.IsNullOrEmpty(value) || value.Trim().Length == 0)
+				// Create the new child directory.
+				directory = new DirectoryInfo(Path.Combine(directory.FullName, relativeDirectory));
+				
+				// Create the directory if we were requested to create it and it doesn't exist.
+				if (create && !directory.Exists)
 				{
-					storageName = null;
+					directory.Create();
 				}
-
-				// Check for invalid key
-				if (!ValidateStorageKey.IsMatch(value))
-				{
-					throw new Exception("StorageName may only have letters, numbers, and spaces.");
-				}
-
-				// Set it, while trimming. Leading and trailing spaces are
-				// bad in general and make life harder. So we do it silently.
-				storageName = value.Trim();
-				Initialize();
 			}
+			
+			// Return the resulting directory.
+			return directory;
 		}
-
-		#endregion
-
-		#region Storage Directory
-
-		/// <summary>
-		/// Returns a DirectoryInfo representing the storage area. The
-		/// directory is not ensured to be created, unless the
-		/// Initialize() method is called.
-		/// </summary>
-		public DirectoryInfo StorageDirectory
-		{
-			get
-			{
-				string directoryName = string.Format("{0}{1}{2}",
-				                                     Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-				                                     Path.DirectorySeparatorChar,
-				                                     StorageName);
-				return new DirectoryInfo(directoryName);
-			}
-		}
-
-		/// <summary>
-		/// Creates the top-level directory for storage. This assumes that
-		/// the storage key is defined.
-		/// </summary>
-		public void Initialize()
-		{
-			// If we don't have a group, then ignore it.
-			if (storageName == null)
-			{
-				return;
-			}
-
-			// Create it
-			DirectoryInfo dir = StorageDirectory;
-
-			if (!dir.Exists)
-			{
-				Log.Info("Created storage area: {0}", dir.FullName);
-				dir.Create();
-			}
-		}
-
-		/// <summary>
-		/// Returns a directory to represent a subapplication inside the
-		/// system. This assumes that the storage directory has already
-		/// been created, but it will create folders if they have not
-		/// been. This automatically creates the folder for the
-		/// application.
-		/// </summary>
-		public DirectoryInfo GetDirectory(string appName)
-		{
-			// Cause problems if we get a null
-			if (appName == null)
-			{
-				throw new Exception("Cannot create a null application");
-			}
-
-			// Check for invalid key
-			if (!ValidateStorageKey.IsMatch(appName))
-			{
-				throw new Exception("Application names may only have letters, numbers, and spaces.");
-			}
-
-			// Get the path
-			string path = StorageDirectory.FullName + Path.DirectorySeparatorChar + appName;
-			var dir = new DirectoryInfo(path);
-
-			// Create it if it doesn't exist
-			if (!dir.Exists)
-			{
-				Log.Info("Creating application folder: {0}", path);
-				dir.Create();
-			}
-
-			// Return it
-			return dir;
-		}
-
-		#endregion
-
-		#region Logging
-
-		private Log internalLog;
-
-		/// <summary>
-		/// Gets the logging interface for this class.
-		/// </summary>
-		/// <value>The log.</value>
-		private Log Log
-		{
-			get
-			{
-				if (internalLog == null)
-				{
-					internalLog = new Log(this);
-				}
-
-				return internalLog;
-			}
-		}
-
-		#endregion
 	}
 }
