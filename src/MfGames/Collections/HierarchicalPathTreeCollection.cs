3e31c535548b91d26bf3fc5a3c80625d0c61a592
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2011-03-25 21:47:14 -0500

    Added the basic tree collection.

---
+++ src/MfGames/Collections/HierarchicalPathTreeCollection.cs
@@ -0,0 +1,144 @@
+#region Copyright and License
+
+// Copyright (c) 2005-2011, Moonfire Games
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#endregion
+
+#region Namespaces
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace MfGames.Collections
+{
+	/// <summary>
+	/// Represents a collection of items, keyed by a <see cref="HierarchicalPath"/>
+	/// and arranged into a tree view.
+	/// </summary>
+	public class HierarchicalPathTreeCollection<TValue>
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="HierarchicalPathTreeCollection&lt;TValue&gt;"/> class.
+		/// </summary>
+		public HierarchicalPathTreeCollection()
+		{
+			children = new Dictionary<string, HierarchicalPathTreeCollection<TValue>>();
+		}
+
+		#endregion
+
+		#region Factory
+
+		private readonly Dictionary<string, HierarchicalPathTreeCollection<TValue>>
+			children;
+
+		/// <summary>
+		/// Gets the count of child items.
+		/// </summary>
+		public int Count
+		{
+			get { return children.Count; }
+		}
+
+		/// <summary>
+		/// Creates the child collection. This is called when the tree needs to
+		/// create a child tree.
+		/// </summary>
+		/// <returns></returns>
+		protected virtual HierarchicalPathTreeCollection<TValue> CreateChild()
+		{
+			return new HierarchicalPathTreeCollection<TValue>();
+		}
+
+		#endregion
+
+		#region Collection
+
+		/// <summary>
+		/// Retrieves the collection at a given path.
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		public HierarchicalPathTreeCollection<TValue> Get(HierarchicalPath path)
+		{
+			// If we are at the top-level path, return ourselves.
+			if (path.Count == 0)
+			{
+				return this;
+			}
+
+			// Get the top-level element for a child.
+			string topLevel = path.First;
+			var childPath = new HierarchicalPath(path.Levels, 1, true);
+
+			return children[topLevel].Get(childPath);
+		}
+
+		/// <summary>
+		/// Adds an item at the specified path.
+		/// </summary>
+		/// <param name="path">The path.</param>
+		/// <param name="item">The item.</param>
+		public void Add(
+			HierarchicalPath path,
+			TValue item)
+		{
+			// If are the top-level path, set the item.
+			if (path.Count == 0)
+			{
+				Item = item;
+				return;
+			}
+
+			// We have at least one more level. Figure out the top-level string
+			// and create the key for it.
+			string topLevel = path.First;
+
+			if (!children.ContainsKey(topLevel))
+			{
+				children[topLevel] = CreateChild();
+			}
+
+			// Pull out the child tree so we can add it.
+			HierarchicalPathTreeCollection<TValue> child = children[topLevel];
+			var childPath = new HierarchicalPath(path.Levels, 1, true);
+
+			child.Add(childPath, item);
+		}
+
+		#endregion
+
+		#region Item
+
+		/// <summary>
+		/// Gets or sets the item at this level.
+		/// </summary>
+		/// <value>
+		/// The item.
+		/// </value>
+		public TValue Item { get; set; }
+
+		#endregion
+	}
+}
---
+++ src/UnitTests/HierarchicalPathTreeCollectionTests.cs
@@ -0,0 +1,114 @@
+#region Copyright and License
+
+// Copyright (c) 2005-2011, Moonfire Games
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#endregion
+
+#region Namespaces
+
+using MfGames;
+using MfGames.Collections;
+
+using NUnit.Framework;
+
+#endregion
+
+namespace UnitTests
+{
+	/// <summary>
+	/// Test the <see cref="HierarchicalPathTreeCollection{TValue}"/> functionality.
+	/// </summary>
+	[TestFixture]
+	public class HierarchicalPathTreeCollectionTests
+	{
+		/// <summary>
+		/// Tests the state of an empty collection.
+		/// </summary>
+		[Test]
+		public void TestEmpty()
+		{
+			// Setup
+			var collection = new HierarchicalPathTreeCollection<int>();
+
+			// Operation
+			
+			// Verification
+			Assert.AreEqual(0, collection.Count);
+		}
+
+		/// <summary>
+		/// Tests adding a single item to the root path.
+		/// </summary>
+		[Test]
+		public void TestRootAdd()
+		{
+			// Setup
+			var collection = new HierarchicalPathTreeCollection<int>();
+			var path = new HierarchicalPath("/");
+
+			// Operation
+			collection.Add(path, 234);
+
+			// Verification
+			Assert.AreEqual(0, collection.Count);
+			Assert.AreEqual(234, collection.Item);
+		}
+		
+		/// <summary>
+		/// Tests adding item at depth.
+		/// </summary>
+		[Test]
+		public void TestDepthAdd()
+		{
+			// Setup
+			var collection = new HierarchicalPathTreeCollection<int>();
+			var path = new HierarchicalPath("/a/b");
+
+			// Operation
+			collection.Add(path, 234);
+
+			// Verification
+			Assert.AreEqual(1, collection.Count);
+			Assert.AreEqual(0, collection.Item);
+			Assert.AreEqual(234, collection.Get(path).Item);
+		}
+
+		[Test]
+		public void TestMultipleDepthAdds()
+		{
+			// Setup
+			var collection = new HierarchicalPathTreeCollection<int>();
+			var path1 = new HierarchicalPath("/a/b");
+			var path2 = new HierarchicalPath("/a/c");
+
+			// Operation
+			collection.Add(path1, 234);
+			collection.Add(path2, 567);
+
+			// Verification
+			Assert.AreEqual(1, collection.Count);
+			Assert.AreEqual(0, collection.Item);
+			Assert.AreEqual(2, collection.Get(new HierarchicalPath("/a")).Count);
+			Assert.AreEqual(234, collection.Get(path1).Item);
+			Assert.AreEqual(567, collection.Get(path2).Item);
+		}
+	}
+}
