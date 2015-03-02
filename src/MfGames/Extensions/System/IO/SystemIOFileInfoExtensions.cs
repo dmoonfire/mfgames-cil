c9e36895290fd61cb18c383fda51821d7e5eefb3
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2015-03-02 13:30:27 -0600

    Adding FileInfo.GetSha256HexString() extension method.

--- src/MfGames/Extensions/System/IO/SystemIOFileInfoExtensions.cs
+++ src/MfGames/Extensions/System/IO/SystemIOFileInfoExtensions.cs
@@ -5,73 +5,75 @@
 //   MIT License (MIT)
 // </license>
 
+using System.IO;
+using System.Security.Cryptography;
+
 namespace MfGames.Extensions.System.IO
 {
-#if NET45
-    using global::System.IO;
+	/// <summary>
+	/// Extensions for System.IO.FileInfo.
+	/// </summary>
+	public static class SystemIOFileInfoExtensions
+	{
+		#region Public Methods and Operators
 
-    using global::System.Security.Cryptography;
+		/// <summary>
+		/// Gets the basename.
+		/// </summary>
+		/// <param name="file">
+		/// The file.
+		/// </param>
+		/// <returns>
+		/// </returns>
+		public static string GetBasename(this FileInfo file)
+		{
+			string name = file.Name.Replace(
+				file.Extension,
+				string.Empty);
+			return name;
+		}
 
-    /// <summary>
-    /// Extensions for System.IO.FileInfo.
-    /// </summary>
-    public static class SystemIOFileInfoExtensions
-    {
-        #region Public Methods and Operators
+		/// <summary>
+		/// Hashes the file with SHA256 and returns the results as a byte
+		/// array.
+		/// </summary>
+		/// <param name="file">
+		/// The file.
+		/// </param>
+		/// <returns>
+		/// </returns>
+		public static byte[] GetSha256(this FileInfo file)
+		{
+			// If it doesn't exist, throw an exception.
+			if (!file.Exists)
+			{
+				throw new FileNotFoundException(
+					"Cannot find file to hash.",
+					file.FullName);
+			}
 
-        /// <summary>
-        /// Gets the basename.
-        /// </summary>
-        /// <param name="file">
-        /// The file.
-        /// </param>
-        /// <returns>
-        /// </returns>
-        public static string GetBasename(this FileInfo file)
-        {
-            string name = file.Name.Replace(
-                file.Extension, 
-                string.Empty);
-            return name;
-        }
+			// Open up a stream in a way that lets others also read it.
+			using (FileStream stream = file.Open(
+				FileMode.Open,
+				FileAccess.Read,
+				FileShare.Read))
+			{
+				using (var buffered = new BufferedStream(stream))
+				{
+					using (var hasher = new SHA256Managed())
+					{
+						byte[] results = hasher.ComputeHash(buffered);
+						return results;
+					}
+				}
+			}
+		}
 
-        /// <summary>
-        /// Hashes the file with SHA256 and returns the results as a byte
-        /// array.
-        /// </summary>
-        /// <param name="file">
-        /// The file.
-        /// </param>
-        /// <returns>
-        /// </returns>
-        public static byte[] GetSha256(this FileInfo file)
-        {
-            // If it doesn't exist, throw an exception.
-            if (!file.Exists)
-            {
-                throw new FileNotFoundException(
-                    "Cannot find file to hash.", 
-                    file.FullName);
-            }
+		public static string GetSha256HexString(this FileInfo file)
+		{
+			return file.GetSha256().ToHexString();
+		}
 
-            // Open up a stream in a way that lets others also read it.
-            using (FileStream stream = file.Open(
-                FileMode.Open, 
-                FileAccess.Read, 
-                FileShare.Read))
-            {
-                using (var buffered = new BufferedStream(stream))
-                {
-                    using (var hasher = new SHA256Managed())
-                    {
-                        byte[] results = hasher.ComputeHash(buffered);
-                        return results;
-                    }
-                }
-            }
-        }
-
-        #endregion
-    }
-#endif
+		#endregion
+	}
 }
